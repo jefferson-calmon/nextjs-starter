1ea63d831db9605224c6db0a428948d48fc0a87e
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-05-22 16:51:11 -0300

    :rocket: feat:  Add a new hook called `useError` to handle and catch errors

---
+++ src/hooks/useError.ts
@@ -0,0 +1,96 @@
+import { useMemo, useState } from 'react';
+import { errors as firebaseErrors } from 'config/firebase/errors';
+
+interface Error {
+	id: string;
+	message: string;
+}
+
+function createErrorFromString(error: string): Error {
+	return {
+		id: error.slugify(),
+		message: error,
+	};
+}
+
+export function useError() {
+	// States
+	const [errors, setErrors] = useState<Error[]>([]);
+	const [lastError, setLastError] = useState<Error | undefined>();
+
+	// Memo vars
+	const exists = useMemo(() => errors.length > 0, [errors]);
+
+	// Functions
+	function clear() {
+		setErrors([]);
+		setLastError(undefined);
+	}
+
+	function add(error: Error | string) {
+		setErrors((prev) => {
+			const errors = Array.from(prev);
+			const isString = typeof error === 'string';
+
+			const errorObject = isString ? createErrorFromString(error) : error;
+			const index = errors.findIndex((err) => err.id === errorObject.id);
+
+			if (index >= 0) errors[index] = errorObject;
+			else errors.push(errorObject);
+
+			setLastError(errorObject);
+			return errors;
+		});
+	}
+
+	function get(id: string) {
+		return errors.find((error) => error.id === id);
+	}
+
+	function remove(id: string) {
+		setErrors((prev) => prev.filter((error) => error.id !== id));
+	}
+
+    function update(id: string, message: string) {
+        setErrors((prev) => {
+            const errors = Array.from(prev);
+            const index = errors.findIndex((error) => error.id === id);
+
+            if (index >= 0) errors[index].message = message;
+            return errors;
+        });
+    }
+
+	function catcher(error: unknown) {
+		const err = error as Record<string, string>;
+		const code = err.code;
+		const message = err.message;
+
+		console.log('[+] Error in `useError.catcher`', error);
+
+		if (!code) {
+			add({
+				id: 'process',
+				message: `Houve um erro durante o processo. Error: ${message}.`,
+			});
+		} else {
+			add({
+				id: code,
+				message: firebaseErrors[code],
+			});
+		}
+	}
+
+	return {
+        exists,
+		errors,
+		lastError,
+
+		catcher,
+		clear,
+		add,
+		get,
+		remove,
+        update,
+	};
+}
