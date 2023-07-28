dbe3a2e003d43e613c6f5f9dd24cf472c4554ac7
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-07-27 23:12:24 -0300

    :sparkles: feat(packages): Update all outdated packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+    compiler: {
+        styledComponents: true,
+    },
     reactStrictMode: true,
     images: {
         domains: ['ui-avatars.com', 'firebasestorage.googleapis.com'],
