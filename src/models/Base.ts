cce563fffcc5509906c2f2a50dcb10d08cb728ab
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-07-27 22:39:12 -0300

    :sparkles: feat(zod): Change base model and start to use zod

--- src/models/Base.ts
+++ src/models/Base.ts
@@ -1,9 +1,22 @@
-export interface BaseModel {
-	id: string;
+import { uuid } from 'codekit';
+import { z } from 'zod';
 
-	createdAt: string;
-	updatedAt: string;
-}
+export const BaseModelSchema = z.object({
+	id: z
+		.string()
+		.default('')
+		.transform(() => uuid()),
+	createdAt: z
+		.string()
+		.default('')
+		.transform(() => new Date().toISOString()),
+	updatedAt: z
+		.string()
+		.default('')
+		.transform(() => new Date().toISOString()),
+});
+
+export type BaseModel = z.infer<typeof BaseModelSchema>;
 
 export type WithoutBaseProps<T> = Omit<T, 'id' | 'updatedAt' | 'createdAt'>;
-export type WithoutTimeProps<T> = Omit<T, 'updatedAt' | 'createdAt'>
+export type WithoutTimeProps<T> = Omit<T, 'updatedAt' | 'createdAt'>;
