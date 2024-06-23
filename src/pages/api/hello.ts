050eb4f5b7c8659c97c1ea3e40b6a9c6df4ca52d
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-06-23 09:44:18 -0300

    :hammer: refactor(api/hello): Update base initial api template `hello`

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { withCatcher } from 'middlewares/catcher';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+	return res.status(200).json({ name: 'John Doe' });
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default withCatcher(handler);
