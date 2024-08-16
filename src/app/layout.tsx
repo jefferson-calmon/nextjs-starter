34001853acd225bf37813b0229920cae3c2fe60a
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-08-15 22:57:08 -0300

    :sparkles: feat(app/layout): Enhance metadata for improved SEO and social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,43 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
 	title: [app.name].join(' - '),
 	description: app.description,
+	keywords: app.keywords,
+	themeColor: '#ffffff',
+	authors: [{ name: app.author.name, url: app.author.url }],
+	robots: 'index, follow',
+	openGraph: {
+		type: 'website',
+		url: `https://${app.site}`,
+		siteName: app.site,
+		title: app.name,
+		description: app.description,
+		images: ['/og.png'],
+	},
+	twitter: {
+		card: 'summary_large_image',
+		site: app.site,
+		title: app.name,
+		description: app.description,
+		images: ['/og.png'],
+	},
+	icons: {
+		icon: [
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				url: '/favicon-32x32.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				url: '/favicon-16x16.png',
+			},
+		],
+		apple: '/apple-touch-icon.png',
+	},
+	manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
