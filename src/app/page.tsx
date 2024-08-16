151b2e3a0082f95d83645852134d5ce0037125a3
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-08-16 02:52:28 -0300

    :sparkles: feat(styles): Add Sass support and improve styling for the Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import styles from './styles.module.scss';
+
 export default function Home() {
 	return (
-		<h1>
-			Hello World <button>Teste</button>
-		</h1>
+		<div className={styles.container}>
+			<h1>Hello World</h1>
+		</div>
 	);
 }
