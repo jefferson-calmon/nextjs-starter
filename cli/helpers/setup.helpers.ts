4d233e82801c3efa213a8d3be34b4fab7922e3a4
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-08-16 23:14:19 -0300

    :sparkles: feat(helpers/setup): Add exec utility to open created API file in editor

--- cli/helpers/setup.helpers.ts
+++ cli/helpers/setup.helpers.ts
@@ -5,6 +5,7 @@ import { z } from 'zod';
 import { SetupSchema } from '../commands/setup.ts';
 import { createFile } from './index.ts';
 import { ApiService } from '../templates/services/api.ts';
+import { exec } from '../utils/exec.ts';
 
 type Item = z.infer<typeof SetupSchema>[0];
 // type Options = z.infer<typeof SetupSchema>[1];
@@ -20,9 +21,11 @@ export function createSetup(item: Item) {
 }
 
 async function apiSetupAction() {
-	await createFile({
+	const result = await createFile({
 		dir: path.join(path.dirname(''), 'services'),
 		name: 'api.ts',
 		content: ApiService(),
 	});
+
+	await exec(`code ${result.path}`);
 }
