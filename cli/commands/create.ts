bcd1d0dfe0f1a7b4f1aeb86bed15bcbd7e358af7
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-08-16 23:07:47 -0300

    :sparkles: feat(cli/commands): Refactor file creation process by introducing `createFile` helper function.

--- cli/commands/create.ts
+++ cli/commands/create.ts
@@ -1,12 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import * as prompt from '@clack/prompts';
-import chalk from 'chalk';
 import { z } from 'zod';
-import { join } from 'path';
 
 import * as H from '../helpers/create.helpers.ts';
-import { writeFile } from '../utils/fs.ts';
 import { exec } from '../utils/exec.ts';
+import { createFile } from '../helpers/index.ts';
 
 export const CreateSchema = z.tuple([
 	z.enum(['model', 'component', 'context']),
@@ -19,27 +16,11 @@ export async function create(...args: any[]) {
 	const [item, name] = CreateSchema.parse(args);
 
 	const files = H.getFiles(item, name);
-
-	const loading = prompt.spinner();
 	const lastFile = files[files.length - 1];
 
 	for (const file of files) {
-		const [name, dir, path] = [
-			chalk.cyan(file.name),
-			chalk.magenta(file.dir),
-			join(file.dir, file.name),
-		];
-
-		loading.start(`Criando arquivo ${name} em ${dir}`);
-
-		const { success } = await writeFile(path, file.content);
-
-		const message = success
-			? 'foi criado com sucesso'
-			: 'falhou ao ser criado';
-
-		if (file === lastFile) await exec(`code ${path}`);
-
-		loading.stop(`O arquivo ${name} ${message} em ${dir}`);
+		await createFile(file).then(async (result) => {
+			if (file === lastFile) await exec(`code ${result.path}`);
+		});
 	}
 }
---
+++ cli/helpers/index.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import { spinner } from '@clack/prompts';
+import { join } from 'path';
+import { writeFile } from '../utils/fs.ts';
+
+export interface AppFile {
+	name: string;
+	dir: string;
+	content: string;
+}
+
+export async function createFile(file: AppFile) {
+	const loading = spinner();
+
+	const [name, dir, path] = [
+		chalk.cyan(file.name),
+		chalk.magenta(file.dir),
+		join(file.dir, file.name),
+	];
+
+	loading.start(`Criando arquivo ${name} em ${dir}`);
+
+	const { success, error } = await writeFile(path, file.content);
+
+	const message = success ? 'foi criado com sucesso' : 'falhou ao ser criado';
+
+	loading.stop(`O arquivo ${name} ${message} em ${dir}`);
+
+	return {
+		success,
+		error,
+		path,
+	};
+}
