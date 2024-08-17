6f1ecf7b3fbb2347628c24d325c91e3241548f72
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-08-16 23:23:37 -0300

    :sparkles: feat(services/api): Update `api` function to prepend '/api' to the path for better routing.

--- cli/templates/services/api.ts
+++ cli/templates/services/api.ts
@@ -1,7 +1,7 @@
 export const ApiService =
 	() => `export function api(path: string, init?: RequestInit) {
 	const baseUrl = '';
-	const url = new URL(path, baseUrl);
+	const url = new URL('/api'.concat(path), baseUrl);
 
 	return fetch(url, init);
 }
