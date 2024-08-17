f8c456dc239fd93b1693460740d45a2835f6dd6b
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-08-17 02:03:00 -0300

    :sparkles: feat(config/env): Introduce `@t3-oss/env-nextjs` for environment variable management

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,14 @@
+import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
-export const env = z
-	.object({
-		NEXT_PUBLIC_BASE_URL: z.string().url(),
-	})
-	.parse(process.env);
+export const env = createEnv({
+	server: {
+		APP_URL: z.string().url(),
+	},
+
+	client: {},
+
+	runtimeEnv: {
+		APP_URL: process.env.APP_URL,
+	},
+});
