8cae63f03b106b0ba39b91743fc00c6a6a2b188c
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-09-04 15:12:07 -0300

    :sparkles: feat(components/Link): Introduce a new `Link` component with routing capabilities.

---
+++ src/components/Link/index.tsx
@@ -0,0 +1,29 @@
+import { useMemo } from 'react';
+import { Link as NBLink, LinkProps as NBLinkProps } from 'next-bricks';
+
+import { Route, routes } from 'config/routes';
+import { cn } from 'utils/cn';
+import styles from './styles.module.scss';
+
+export interface LinkProps extends NBLinkProps {
+	to?: string & Route;
+}
+
+function Link(props: LinkProps) {
+	// Memo vars
+	const linkTo = useMemo(() => {
+		if (!props.to) return props.to;
+
+		const route = routes.get(props.to);
+
+		return route || props.to;
+	}, [props.to]);
+
+	return (
+		<div className={cn('link', styles.link)}>
+			<NBLink {...props} to={linkTo} />
+		</div>
+	);
+}
+
+export default Link;
