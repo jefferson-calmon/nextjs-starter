164a7b56e705b3e578ceafde8ef6975d13ac9444
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-09-04 15:09:57 -0300

    :sparkles: feat(components/Input): Introduce a new `Input` component with basic styling.

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,16 @@
+import { Input as NBInput, InputProps as NBInputProps } from 'next-bricks';
+
+import { cn } from 'utils/cn';
+import styles from './styles.module.scss';
+
+export interface InputProps extends NBInputProps {}
+
+function Input(props: InputProps) {
+	return (
+		<div className={cn('input', styles.input)}>
+			<NBInput {...props} />
+		</div>
+	);
+}
+
+export default Input;
