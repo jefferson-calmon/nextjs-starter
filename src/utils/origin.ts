62a7b0ab588f332bf29840ed1db8a11261516b6e
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-09-05 14:35:37 -0300

    :sparkles: feat(utils/origin): Introduce `getOrigin` function to retrieve the origin URL based on the environment.

---
+++ src/utils/origin.ts
@@ -0,0 +1,21 @@
+const ENV_URLS = {
+	development: '',
+	production: '',
+	testing: '',
+};
+
+const DEFAULT_URL = 'http://localhost:3000';
+
+export function getOrigin(): string {
+	const isBrowser = typeof window !== 'undefined';
+	const url = isBrowser
+		? window.location.origin
+		: process.env.URL || process.env.VERCEL_URL || DEFAULT_URL;
+
+	if (process.env.NEXT_PUBLIC_ENVIRONMENT) {
+		const environment = process.env.NEXT_PUBLIC_ENVIRONMENT;
+		return ENV_URLS[environment as keyof typeof ENV_URLS] || url;
+	}
+
+	return url.includes('http') ? url : `https://${url}`;
+}
