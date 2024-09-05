daa721ca1cca45b563ef0214dabc9151ffaa1d27
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-09-05 14:14:19 -0300

    :sparkles: feat(navigation): Add `nextjs-toploader` for improved navigation progress bar

--- src/components/NavigationProgressBar/index.tsx
+++ src/components/NavigationProgressBar/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { NavigationProgressBar as NBNavigationProgressBar } from 'next-bricks';
+import NextTopLoader from 'nextjs-toploader';
 
 import { useTheme } from 'contexts/ThemeContext';
 
@@ -8,7 +8,16 @@ function NavigationProgressBar() {
 	// Hooks
 	const theme = useTheme();
 
-	return <NBNavigationProgressBar color={theme.colors.primary} />;
+	return (
+		<NextTopLoader
+			color={theme.colors.primary}
+			initialPosition={0.3}
+			crawl={true}
+			crawlSpeed={200}
+			height={2.5}
+			showSpinner={true}
+		/>
+	);
 }
 
 export default NavigationProgressBar;
